7452063384fbc24831abc565d965a16e8e9f07cd
Saikat Das <saikatdas40g@gmail.com>
2024-01-24 18:23:52 +0530

    updated backend

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({credentials: true}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
