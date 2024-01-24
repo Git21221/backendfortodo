2b8dcad5d7c1742791a8f34cad5c39b8025e1b3b
Saikat Das <saikatdas40g@gmail.com>
2024-01-24 16:50:21 +0530

    updated backend

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,17 +1,12 @@
-import multer from 'multer';
-import path from 'path';
+import multer from "multer";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, 'public'));
+    cb(null, "/public");
   },
   filename: function (req, file, cb) {
-    // Use a unique filename to avoid conflicts
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-    cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
+    cb(null, file.originalname);
   },
 });
 
-const upload = multer({ storage: storage });
-
-export { upload };
+export const upload = multer({ storage: storage });
