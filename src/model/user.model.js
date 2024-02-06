32b84619dfc3c566f2b01dd40a6e4dd93d7e7068
Saikat Das <saikatdas40g@gmail.com>
2024-02-06 17:49:57 +0530

    updated backend

--- src/model/user.model.js
+++ src/model/user.model.js
@@ -32,7 +32,7 @@ const userSchema = new Schema(
     dateofBirth: {
       type: String,
     },
-    monthofBirth:{
+    monthofBirth: {
       type: String,
     },
     yearofBirth: {
@@ -59,7 +59,7 @@ userSchema.methods.isPasswordCorrect = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
 
-userSchema.methods.generateAccessToken = async function () {
+userSchema.methods.generateAccessToken = function () {
   return jwt.sign(
     {
       _id: this._id,
@@ -68,11 +68,11 @@ userSchema.methods.generateAccessToken = async function () {
       fullName: this.fullName,
     },
     process.env.ACCESS_TOKEN_SECRET,
-    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY}
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
   );
 };
 
-userSchema.methods.generateRefreshToken = async function () {
+userSchema.methods.generateRefreshToken = function () {
   return jwt.sign(
     {
       _id: this._id,
